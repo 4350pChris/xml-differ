1b421b056b9ed77b69e6c03dd7336a61b94404e4
Chris-Robin Ennen <chris-robin@ennen.dev>
2025-06-24 17:12:13 +0200

    fix: proper return type for diff

--- api/routers/diff.py
+++ api/routers/diff.py
@@ -1,4 +1,4 @@
-from typing import Annotated
+from typing import Annotated, List
 
 from fastapi import APIRouter, HTTPException, Depends
 
@@ -20,7 +20,7 @@ async def get_diff(
     left_version_id: str,
     right_version_id: str,
     diff_strategy: Annotated[DiffStrategy, Depends(get_diff_strategy)],
-):
+) -> List[Diff]:
     left = await LawVersion.get(left_version_id, fetch_links=True)
     if left is None:
         raise HTTPException(status_code=404, detail="Left version not found")
@@ -28,19 +28,13 @@ async def get_diff(
     if right is None:
         raise HTTPException(status_code=404, detail="Right version not found")
 
-    diffs = []
-    for paragraph in left.paragraphs:
-        matching_paragraph = next(
-            p for p in right.paragraphs if p.title == paragraph.title
+    return [
+        Diff(
+            left_index=left.index,
+            right_index=right.index,
+            edits=diff_strategy(left.content, right.content),
         )
-        if matching_paragraph:
-            edits = diff_strategy(paragraph.content, matching_paragraph.content)
-            diffs.append(
-                Diff(
-                    left_index=paragraph.index,
-                    right_index=matching_paragraph.index,
-                    edits=edits,
-                )
-            )
-
-    return diffs
+        for left in left.paragraphs
+        for right in right.paragraphs
+        if left.title == right.title
+    ]
