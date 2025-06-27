13ea3417197f254f1b953a1e101701086af71803
Chris-Robin Ennen <chris-robin@ennen.dev>
2025-06-27 15:15:25 +0200

    feat: init frontend

---
+++ frontend/vite.config.ts
@@ -0,0 +1,19 @@
+import md from "unplugin-vue-markdown/vite";
+import vue from "@vitejs/plugin-vue";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+import vike from "vike/plugin";
+
+export default defineConfig({
+  plugins: [
+    vike({}),
+    tailwindcss(),
+    vue({
+      include: [/\.vue$/, /\.md$/],
+    }),
+    md({}),
+  ],
+  build: {
+    target: "es2022",
+  },
+});
