9c315b4714d71472c35c9be5a625d0d51bb5ffda
Chris-Robin Ennen <chris-robin@ennen.dev>
2025-07-01 15:27:50 +0200

    feat: add back button to nav

--- frontend/components/Navbar.vue
+++ frontend/components/Navbar.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
 import { usePageContext } from "vike-vue/usePageContext";
 import { getTitle } from "../pages/utils";
+import { computed } from "vue";
+import Link from "./Link.vue";
 
 const pageContext = usePageContext();
 const title = getTitle(pageContext);
+const isRootPath = computed(() => pageContext.urlPathname === "/");
 </script>
 
 <template>
@@ -11,7 +14,15 @@ const title = getTitle(pageContext);
     class="bg-base-100/90 text-base-content sticky top-0 z-30 flex h-16 w-full [transform:translate3d(0,0,0)] justify-center backdrop-blur transition-shadow duration-100 print:hidden shadow-xs"
   >
     <div class="navbar w-full py-0">
-      <h1 class="text-2xl">{{ title }}</h1>
+      <div class="flex items-center gap-2">
+        <Link v-if="!isRootPath" class="btn btn-ghost text-xl" href="/">
+          <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
+            <!-- Icon from Google Material Icons by Material Design Authors - https://github.com/material-icons/material-icons/blob/master/LICENSE -->
+            <path fill="currentColor" d="M17.77 3.77L16 2L6 12l10 10l1.77-1.77L9.54 12z" />
+          </svg>
+        </Link>
+        <h1 class="text-2xl">{{ title }}</h1>
+      </div>
     </div>
   </div>
 </template>
