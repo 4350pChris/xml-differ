7805e5ab510778c7af894eea164b41950ef2103a
Chris-Robin Ennen <chris-robin@ennen.dev>
2025-07-01 16:05:13 +0200

    feat: add search for laws

---
+++ frontend/components/LawSearch.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import { useAllLaws } from "../composables/useAllLaws";
+import { computed, ref } from "vue";
+import LawList from "./LawList.vue";
+
+const { data: laws } = useAllLaws();
+const searchQuery = ref("");
+
+const filteredLaws = computed(() => {
+  if (!laws.value || !searchQuery.value) return null;
+  const query = searchQuery.value.toLowerCase();
+  return laws.value.filter(
+    (law) =>
+      law.name.toLowerCase().includes(query) ||
+      (law.short_title && law.short_title.toLowerCase().includes(query)) ||
+      (law.long_title && law.long_title.toLowerCase().includes(query)),
+  );
+});
+</script>
+
+<template>
+  <div class="dropdown">
+    <label class="input">
+      <svg class="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+        <g stroke-linejoin="round" stroke-linecap="round" stroke-width="2.5" fill="none" stroke="currentColor">
+          <circle cx="11" cy="11" r="8"></circle>
+          <path d="m21 21-4.3-4.3"></path>
+        </g>
+      </svg>
+      <input
+        v-model="searchQuery"
+        type="search"
+        spellcheck="false"
+        autocomplete="off"
+        class="grow"
+        placeholder="Search"
+      />
+      <kbd class="kbd kbd-sm">⌘</kbd>
+      <kbd class="kbd kbd-sm">K</kbd>
+    </label>
+    <LawList
+      v-if="filteredLaws"
+      tabindex="0"
+      class="dropdown-content rounded-box z-1 w-64 max-h-96 overflow-y-auto p-2 shadow-sm"
+      :laws="filteredLaws"
+    />
+  </div>
+</template>
--- frontend/components/Navbar.vue
+++ frontend/components/Navbar.vue
@@ -3,6 +3,7 @@ import { usePageContext } from "vike-vue/usePageContext";
 import { getTitle } from "../pages/utils";
 import { computed } from "vue";
 import Link from "./Link.vue";
+import LawSearch from "./LawSearch.vue";
 
 const pageContext = usePageContext();
 const title = getTitle(pageContext);
@@ -13,7 +14,7 @@ const isRootPath = computed(() => pageContext.urlPathname === "/");
   <div
     class="bg-base-100/90 text-base-content sticky top-0 z-30 flex h-16 w-full [transform:translate3d(0,0,0)] justify-center backdrop-blur transition-shadow duration-100 print:hidden shadow-xs"
   >
-    <div class="navbar w-full py-0">
+    <div class="navbar w-full py-0 gap-4">
       <div class="flex items-center gap-2">
         <Link v-if="!isRootPath" class="btn btn-ghost text-xl" href="/">
           <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
@@ -23,6 +24,7 @@ const isRootPath = computed(() => pageContext.urlPathname === "/");
         </Link>
         <h1 class="text-2xl">{{ title }}</h1>
       </div>
+      <LawSearch />
     </div>
   </div>
 </template>
