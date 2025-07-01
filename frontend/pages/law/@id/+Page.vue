0f6a858cc7fedb8440e3bc373de1584e914d4844
Chris-Robin Ennen <chris-robin@ennen.dev>
2025-07-01 16:02:12 +0200

    refactor: use tanstack for law detail page

--- frontend/pages/law/@id/+Page.vue
+++ frontend/pages/law/@id/+Page.vue
@@ -1,13 +1,25 @@
 <script setup lang="ts">
-import { useData } from "vike-vue/useData";
-import { Data } from "./+data";
 import LawVersionViewer from "../../../components/LawVersionViewer.vue";
+import { useQuery } from "@tanstack/vue-query";
+import { usePageContext } from "vike-vue/usePageContext";
+import { computed, onServerPrefetch } from "vue";
+import { getLawLawsLawIdGetOptions } from "../../../client/@tanstack/vue-query.gen";
 
-const { law } = useData<Data>();
+const pageContext = usePageContext();
+const options = computed(() => {
+  return getLawLawsLawIdGetOptions({
+    path: {
+      law_id: pageContext.routeParams.id as string,
+    },
+  });
+});
+
+const { data: law, suspense } = useQuery(options);
+onServerPrefetch(suspense);
 </script>
 
 <template>
-  <div class="p-4">
+  <div v-if="law" class="p-4">
     <h1 class="text-2xl font-bold">{{ law.name }}</h1>
     <p class="text-gray-700">{{ law.long_title ?? law.short_title }}</p>
     <LawVersionViewer :law />
