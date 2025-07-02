4d607309b84009c00986d51c549e83839057b54b
Chris-Robin Ennen <chris-robin@ennen.dev>
2025-07-02 19:50:31 +0200

    feat: query client config

---
+++ frontend/pages/+queryClientConfig.ts
@@ -0,0 +1,14 @@
+export { queryClientConfig };
+
+import type { QueryClientConfig } from "@tanstack/vue-query";
+
+// Query client options - e.g.
+const queryClientConfig: QueryClientConfig = {
+  defaultOptions: {
+    queries: {
+      // Don't refetch when window loses or gains focus during development
+      refetchOnWindowFocus: import.meta.env.PROD,
+      // staleTime: 1000 * 60 * 5,
+    },
+  },
+};
