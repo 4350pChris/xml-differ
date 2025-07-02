8437504ac5fe92c6f926b716c26b69bc69aba5cd
Chris-Robin Ennen <chris-robin@ennen.dev>
2025-07-02 18:07:47 +0200

    feat: add skeleton loader for law

--- frontend/pages/law/@id/+Page.vue
+++ frontend/pages/law/@id/+Page.vue
@@ -14,14 +14,23 @@ const options = computed(() => {
   });
 });
 
-const { data: law, suspense } = useQuery(options);
+const { data: law, status, suspense } = useQuery(options);
 onServerPrefetch(suspense);
 </script>
 
 <template>
-  <div v-if="law" class="p-4 pl-20 md:pr-12 lg:pr-20">
-    <h1 class="text-2xl font-bold">{{ law.name }}</h1>
-    <p class="text-gray-700">{{ law.long_title ?? law.short_title }}</p>
-    <LawVersionViewer :law />
+  <div class="p-4 pl-20 md:pr-12 lg:pr-20">
+    <template v-if="status === 'pending'">
+      <div class="skeleton h-16 w-24"></div>
+      <div class="skeleton h-8 w-48"></div>
+    </template>
+    <template v-else-if="status === 'error'">
+      <p>Error loading law details!</p>
+    </template>
+    <template v-else-if="law">
+      <h1 class="text-2xl font-bold">{{ law.name }}</h1>
+      <p class="text-gray-700">{{ law.long_title ?? law.short_title }}</p>
+      <LawVersionViewer :law />
+    </template>
   </div>
 </template>
