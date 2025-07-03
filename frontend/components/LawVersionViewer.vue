75b0640b6e02624f7b68005b19d75cdd2900ebcb
Chris-Robin Ennen <chris-robin@ennen.dev>
2025-07-03 17:40:06 +0200

    feat: add input for fast_match

--- frontend/components/LawVersionViewer.vue
+++ frontend/components/LawVersionViewer.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import { LawDetailProjection } from "../client";
 import LawVersionSelector from "./LawVersionSelector.vue";
-import { ref, watch } from "vue";
+import { computed, onServerPrefetch, ref, watch } from "vue";
 import ParagraphsViewer from "./paragraphs/ParagraphsViewer.vue";
 import { useUrlSearchParams } from "@vueuse/core";
 import { usePageContext } from "vike-vue/usePageContext";
+import { getDiffDiffLeftVersionIdRightVersionIdGetOptions } from "../client/@tanstack/vue-query.gen";
+import { useQuery } from "@tanstack/vue-query";
 
 const props = defineProps<{ law: LawDetailProjection }>();
 
@@ -22,6 +24,22 @@ watch(
   },
   { immediate: true },
 );
+
+const queryOptions = computed(() => {
+  console.log("Querying diff for versions:", left.value, right.value, "with fast match:", urlParams.fast);
+  return getDiffDiffLeftVersionIdRightVersionIdGetOptions({
+    path: {
+      left_version_id: left.value,
+      right_version_id: right.value,
+    },
+    query: {
+      fast_match: !!urlParams.fast,
+    },
+  });
+});
+
+const { data: diff, status, suspense } = useQuery(queryOptions);
+onServerPrefetch(suspense);
 </script>
 
 <template>
@@ -29,7 +47,16 @@ watch(
     <div class="flex items-center justify-center pb-2 gap-4">
       <LawVersionSelector v-model="left" legend="Alte Version" :versions="law.versions" />
       <LawVersionSelector v-model="right" legend="Neue Version" :versions="law.versions" />
+      <fieldset class="fieldset bg-base-100 border-base-300 rounded-box w-64 border p-4">
+        <legend class="fieldset-legend">XMLdiff Optionen</legend>
+        <label class="label">
+          <input v-model="urlParams.fast" type="checkbox" class="checkbox" name="fast_match" />
+          Fast Match
+        </label>
+      </fieldset>
     </div>
-    <ParagraphsViewer v-if="right && left" :left :right />
+    <div v-if="status === 'pending'" class="skeleton w-full h-96"></div>
+    <p v-else-if="status === 'error'">Error!</p>
+    <ParagraphsViewer v-else-if="diff" :diff />
   </div>
 </template>
--- frontend/components/paragraphs/ParagraphsViewer.vue
+++ frontend/components/paragraphs/ParagraphsViewer.vue
@@ -1,34 +1,18 @@
 <script setup lang="ts">
 import ParagraphXMLViewer from "./ParagraphXMLViewer.vue";
-import { computed, onServerPrefetch, useTemplateRef } from "vue";
-import { getDiffDiffLeftVersionIdRightVersionIdGetOptions } from "../../client/@tanstack/vue-query.gen";
-import { useQuery } from "@tanstack/vue-query";
+import { useTemplateRef } from "vue";
 import MoveChangeButtons from "./MoveChangeButtons.vue";
 import TableOfContents from "./TableOfContents.vue";
 
-const props = defineProps<{ left: string; right: string }>();
-const queryOptions = computed(() =>
-  getDiffDiffLeftVersionIdRightVersionIdGetOptions({
-    path: {
-      left_version_id: props.left,
-      right_version_id: props.right,
-    },
-  }),
-);
+defineProps<{ diff: string[] }>();
 
-const { data: diff, status, suspense } = useQuery(queryOptions);
-onServerPrefetch(suspense);
 const diffEl = useTemplateRef<HTMLElement>("diffParent");
 </script>
 
 <template>
-  <div v-if="status === 'pending'" class="skeleton w-full h-96"></div>
-  <p v-else-if="status === 'error'">Error!</p>
-  <template v-else>
-    <TableOfContents class="not-prose fixed top-16 bottom-0 left-0 w-20 md:w-24 lg:w-28" :parent-element="diffEl" />
-    <MoveChangeButtons class="fixed bottom-4 right-4" :parent-element="diffEl" />
-    <div ref="diffParent" :key="`${props.left}-${props.right}`">
-      <ParagraphXMLViewer v-for="(content, i) in diff" :key="i" :content />
-    </div>
-  </template>
+  <TableOfContents class="not-prose fixed top-16 bottom-0 left-0 w-20 md:w-24 lg:w-28" :parent-element="diffEl" />
+  <MoveChangeButtons class="fixed bottom-4 right-4" :parent-element="diffEl" />
+  <div ref="diffParent">
+    <ParagraphXMLViewer v-for="(content, i) in diff" :key="`${i}-${content.length}`" :content />
+  </div>
 </template>
