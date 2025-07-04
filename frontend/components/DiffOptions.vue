56d4c36dcb63d2da956fee384c826a642f78bdc9
Chris-Robin Ennen <chris-robin@ennen.dev>
2025-07-04 19:28:35 +0200

    feat: better options styles

--- frontend/components/DiffOptions.vue
+++ frontend/components/DiffOptions.vue
@@ -49,35 +49,40 @@ const ratioOptions: { value: DifferOptions["ratio_mode"]; label: string }[] = [
 </script>
 
 <template>
-  <button class="btn btn-circle" v-bind="$attrs" @click="modalEl?.showModal()">
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+  <button class="btn btn-circle" v-bind="$attrs" title="Optionen" @click="modalEl?.showModal()">
+    <svg xmlns="http://www.w3.org/2000/svg" class="size-6" viewBox="0 0 24 24">
       <!-- Icon from Material Design Icons by Pictogrammers - https://github.com/Templarian/MaterialDesign/blob/master/LICENSE -->
       <path
         fill="currentColor"
-        d="M6 0C4.89 0 4 .89 4 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V6l-6-6zm7 1.5L18.5 7H13zM7 22v2h2v-2zm4 0v2h2v-2zm4 0v2h2v-2z"
+        d="M6 0a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V6l-6-6zm0 2h7v5h5v11H6zm1 20v2h2v-2zm4 0v2h2v-2zm4 0v2h2v-2z"
       />
     </svg>
   </button>
   <dialog ref="modal" class="modal modal-bottom sm:modal-middle" @submit.stop>
     <div class="modal-box">
-      <h3 class="text-lg font-bold">Optionen</h3>
-      <LawVersionSelector v-model="left" legend="Alte Version" :versions="law.versions" />
-      <LawVersionSelector v-model="right" legend="Neue Version" :versions="law.versions" />
-      <fieldset class="fieldset bg-base-100 border-base-300 rounded-box w-64 border p-4">
-        <legend class="fieldset-legend">XMLdiff Optionen</legend>
+      <fieldset class="fieldset bg-base-100 rounded-box p-4">
+        <legend class="fieldset-legend text-lg">Versionen</legend>
+        <LawVersionSelector v-model="left" legend="Alte Version" :versions="law.versions" />
+        <LawVersionSelector v-model="right" legend="Neue Version" :versions="law.versions" />
+      </fieldset>
+      <fieldset class="fieldset bg-base-100 rounded-box p-4">
+        <legend class="fieldset-legend text-lg">XMLdiff</legend>
         <label class="label">
           <input v-model="options.fast_match" type="checkbox" class="checkbox" name="fast_match" />
           Fast Match
         </label>
-        <label class="label">
-          <input v-model="options.F" type="number" class="input" name="F" :min="0" :max="1" :step="0.1" />
-          F (0.0 - 1.0)
+        <label class="input">
+          <input v-model="options.F" type="number" name="F" :min="0" :max="1" :step="0.1" />
+          <span class="label">Ähnlichkeit (0.0 - 1.0)</span>
         </label>
-        <select v-model="options.ratio_mode" name="ratio_mode" class="select">
-          <option v-for="option in ratioOptions" :key="option.value" :value="option.value">
-            {{ option.label }}
-          </option>
-        </select>
+        <label class="select">
+          <select id="ratio_mode" v-model="options.ratio_mode" name="ratio_mode">
+            <option v-for="option in ratioOptions" :key="option.value" :value="option.value">
+              {{ option.label }}
+            </option>
+          </select>
+          <span class="label">Ähnlichkeitsmodus</span>
+        </label>
       </fieldset>
       <div class="modal-action">
         <form method="dialog" @submit="acceptChanges">
