3dfeaba00e47b238d295c2f25ed93370f45d5ed6
Chris-Robin Ennen <chris-robin@ennen.dev>
2025-07-08 09:52:35 +0200

    chore: remove log statement

--- frontend/pages/+onCreateApp.ts
+++ frontend/pages/+onCreateApp.ts
@@ -5,7 +5,6 @@ export const onCreateApp: OnCreateAppSync = (pageContext) => {
   let apiUrl: string;
   if (import.meta.env.SSR) {
     const { ssr, client } = pageContext.config.apiUrl!;
-    console.log(`Using API URL: SSR=${ssr}, Client=${client}`);
     apiUrl = ssr;
     pageContext.apiUrl = client;
   } else {
