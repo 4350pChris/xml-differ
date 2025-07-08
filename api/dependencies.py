50b52a96d9b4183e482ac0bcc5a29b7d64bcf4db
Chris-Robin Ennen <chris-robin@ennen.dev>
2025-07-08 19:08:44 +0200

    fix: use sync redis client for worker, increase timeout

--- api/dependencies.py
+++ api/dependencies.py
@@ -4,15 +4,15 @@ from typing import Annotated
 from fastapi.params import Depends
 from rq import Queue
 
-from redis import asyncio as aioredis
+from redis import Redis
 
 
-def get_redis_connection() -> aioredis.Redis:
+def get_redis_connection() -> Redis:
     redis_url = os.environ.get("REDIS_URL")
     if not redis_url:
         raise ValueError("REDIS_URL environment variable is not set")
-    return aioredis.from_url(redis_url)
+    return Redis.from_url(redis_url)
 
 
-def get_queue(redis_conn: Annotated[aioredis.Redis, Depends(get_redis_connection)]):
+def get_queue(redis_conn: Annotated[Redis, Depends(get_redis_connection)]):
     return Queue(connection=redis_conn)
